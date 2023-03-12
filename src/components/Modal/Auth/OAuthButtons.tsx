f89ef40cc639af7396789d6b671ee93fd4a0296e
meqdad-alhashmi <meqdad000@gmail.com>
2023-03-12 21:33:32 +0200

    users collection

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,10 +1,23 @@
-import { auth } from '@/firbase/clientApp';
+import { auth, firestore } from '@/firbase/clientApp';
 import { Button, Flex, Image, Text } from '@chakra-ui/react';
-import React from 'react';
+import { User } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
+import React, { useEffect } from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 
 const OAuthButtons: React.FC = () => {
-	const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+	const [signInWithGoogle, userCred, loading, error] = useSignInWithGoogle(auth);
+
+	const createUserDocument = async (user: User) => {
+		const userDocRef = doc(firestore, 'users', user.uid);
+		await setDoc(userDocRef, JSON.parse(JSON.stringify(user)));
+	};
+
+	useEffect(() => {
+		if (userCred) {
+			createUserDocument(userCred.user);
+		}
+	}, [userCred]);
 	return (
 		<Flex
 			direction='column'
--- src/components/Modal/Auth/SignUp.tsx
+++ src/components/Modal/Auth/SignUp.tsx
@@ -1,9 +1,11 @@
 import { authModalState } from '@/atoms/AuthModalAtom';
-import { auth } from '@/firbase/clientApp';
+import { auth, firestore } from '@/firbase/clientApp';
 import { Input, Button, Flex, Text } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { User } from 'firebase/auth';
+import { addDoc, collection } from 'firebase/firestore';
 
 const SignUp: React.FC = () => {
 	const setAuthmodalState = useSetRecoilState(authModalState);
@@ -13,7 +15,7 @@ const SignUp: React.FC = () => {
 		confirmPassword: '',
 	});
 	const [error, setError] = useState('');
-	const [createUserWithEmailAndPassword, user, loading, userError] =
+	const [createUserWithEmailAndPassword, userCred, loading, userError] =
 		useCreateUserWithEmailAndPassword(auth);
 
 	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -33,6 +35,19 @@ const SignUp: React.FC = () => {
 			[event.target.name]: event.target.value,
 		}));
 	};
+
+	const createUserDocument = async (user: User) => {
+		await addDoc(
+			collection(firestore, 'users'),
+			JSON.parse(JSON.stringify(user))
+		);
+	};
+
+	useEffect(() => {
+		if (userCred) {
+			createUserDocument(userCred.user);
+		}
+	}, [userCred]);
 
 	return (
 		<form onSubmit={onSubmit}>
